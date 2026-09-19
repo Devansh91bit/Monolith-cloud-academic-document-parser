492c59d581c55bb4ccf9c4a814cfcbc14e2629b6
Devansh <dev91bit@gmail.com>
2026-09-19 16:12:48 +0530

    Fixed HTTP 500 error and /logout endpoint

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse, RedirectResponse
-from dotenv import load_dotenv
-from authlib.integrations.starlette_client import OAuth
-from starlette.middleware.sessions import SessionMiddleware
 import os
+from authlib.integrations.base_client import OAuthError
+from authlib.integrations.starlette_client import OAuth
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.sessions import SessionMiddleware
 
 load_dotenv()
 
@@ -22,45 +23,55 @@ oauth.register(
     client_id=os.getenv("APPID_CLIENT_ID"),
     client_secret=os.getenv("APPID_CLIENT_SECRET"),
     server_metadata_url=os.getenv("APPID_DISCOVERY_ENDPOINT"),
-    client_kwargs={
-        "scope": "openid profile email"
-    }
+    client_kwargs={"scope": "openid profile email"},
 )
+
 
 @app.get("/auth/login")
 async def login(request: Request):
-    return await oauth.appid.authorize_redirect(
-        request,
-        "http://127.0.0.1:8000/auth/callback"
-    )
+  return await oauth.appid.authorize_redirect(
+      request, "http://127.0.0.1:8000/auth/callback"
+  )
+
 
 @app.get("/auth/callback")
 async def auth_callback(request: Request):
+  try:
     token = await oauth.appid.authorize_access_token(request)
-
     request.session["user"] = token.get("userinfo")
-
     return RedirectResponse("/dashboard")
+  except OAuthError:
+    return RedirectResponse("/auth/login")
+
+
+@app.get("/auth/logout")
+async def logout(request: Request):
+  request.session.clear()
+  return RedirectResponse("/")
+
 
 @app.get("/dashboard")
 async def dashboard(request: Request):
-    if "user" not in request.session:
-        return RedirectResponse("/auth/login")
+  if "user" not in request.session:
+    return RedirectResponse("/auth/login")
 
-    return FileResponse("protected/dashboard.html")
+  return FileResponse("protected/dashboard.html")
+
 
 @app.get("/dashboard.css")
 async def dashboard_css(request: Request):
-    if "user" not in request.session:
-        return RedirectResponse("/auth/login")
+  if "user" not in request.session:
+    return RedirectResponse("/auth/login")
 
-    return FileResponse("protected/dashboard.css")
+  return FileResponse("protected/dashboard.css")
+
 
 @app.get("/dashboard.js")
 async def dashboard_js(request: Request):
-    if "user" not in request.session:
-        return RedirectResponse("/auth/login")
+  if "user" not in request.session:
+    return RedirectResponse("/auth/login")
 
-    return FileResponse("protected/dashboard.js")
+  return FileResponse("protected/dashboard.js")
+
 
 app.mount("/", StaticFiles(directory="public", html=True), name="public")
